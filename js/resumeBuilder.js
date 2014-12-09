d0bc5901b0445d0869b3130319124d1ec8f462ed
gnoejuan <gnoejuan@gmail.com>
2014-12-08 20:03:19 -0600

    Signed-off-by: gnoejuan <gnoejuan@gmail.com>

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 var email ="provenhypothesis@gmail.com"
 var name = "Charles Rodriguez"
 var role = "Developer"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
